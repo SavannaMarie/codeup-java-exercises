774a6c3f0ff3f1bdf52719493730339fb4ed33d2
Savanna Marie <savannamarie@gmail.com>
2021-05-24 16:24:56 -0500

    second exercise work

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,16 +6,27 @@ public class ConsoleExercises {
         double pi = 3.14159;
         System.out.printf("The value of pi is approximately: %.2f.%n", pi);
 
-        Scanner newInteger = new Scanner(System.in);
+        Scanner newScanner = new Scanner(System.in);
         System.out.println("Please enter a number.");
-        int userInput = newInteger.nextInt();
+        int userInput = newScanner.nextInt();
         System.out.printf("Your number was %d.%n", userInput);
 
         System.out.println("Please enter 3 words.");
-        String first = newInteger.next();
-        String second = newInteger.next();
-        String third = newInteger.next();
-        System.out.printf("Your 3 words were: %s, %s, and %s%n", first, second, third);
+        String first = newScanner.next();
+        String second = newScanner.next();
+        String third = newScanner.next();
+        System.out.printf("Your 3 words were: %s, %s, and %s", first, second, third);
+
+        newScanner.nextLine();
+        System.out.println("Please enter a sentence:");
+        String sentence = newScanner.nextLine();
+        System.out.printf("your sentence was: %s", sentence);
+        newScanner.nextLine();
+
+
+//        System.out.println("Please enter the length and width of the classroom: ");
+//        String roomLength = newScanner.nextLine();
+//        System.out.printf("Your number was %d.%n", userInput);
 
 
     }

845c986249168d7a1db8865da420f2c69b8f3b8b
Savanna Marie <savannamarie@gmail.com>
2021-05-24 13:18:50 -0500

    complete exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -69,6 +69,59 @@ public class HelloWorld {
         iAmADouble = 3.141598;
         iAmAnInt = (int)iAmADouble;
         System.out.println(iAmAnInt);
+
+        int myFavoriteNumber = 31;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is a string!";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        System.out.println(x);
+//        x = x + 5;
+//        System.out.println(x);
+//        x+= 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println(y);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
     }
 }
 

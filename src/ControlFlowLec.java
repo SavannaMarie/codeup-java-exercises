910201ce7a1d6b9a60a12301b2b54c83d248dae5
Savanna Marie <savannamarie@gmail.com>
2021-05-25 11:25:22 -0500

    switch, loops etc lecture

---
+++ src/ControlFlowLec.java
@@ -0,0 +1,60 @@
+public class ControlFlowLec {
+    public static void main(String[] args) {
+
+//        int x = 5;
+//        if (x == 5){
+//            System.out.println("If you are reading this, it means x is 5");
+//        }
+//        System.out.println("on the other hand, this says nothing about x, because it runs regardless of what x is.");
+
+//        int y = 2;
+//        y = 3;
+//        if (y == 2){
+//            System.out.println("Carried out a conditional test. Can confirm true.");
+//        } else {
+//            System.out.println("I have tested to see if y=2 is true and it is false.");
+//        }
+
+//        short y = -5;
+//        y = 12;
+//        y = 32000;
+//        if (y < 0){
+//            System.out.println("The variable y contains a negative number value");
+//        } else if (y >= 0 && y < 30000){
+//            System.out.println("The variable y contains a positive number value");
+//        } else {
+//            System.out.println("The variably y contains a numeric value that is dangerously close to the max value for short type variables. Which is: " + Short.MAX_VALUE);
+//        }
+
+//        String customerChoice;
+//        customerChoice = "strawberry";
+//        switch (customerChoice){
+//            case "vanilla":
+//                System.out.println("One vanilla coming right up!");
+//                break;
+//            case "chocolate":
+//                System.out.println("Chocolate coming right up");
+//                break;
+//            default:
+//                System.out.println("Wo don't have that!");
+//                break;
+//        }
+
+        boolean existence = true;
+        if (existence){
+            System.out.println("The programmer is a real being");
+        }
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+

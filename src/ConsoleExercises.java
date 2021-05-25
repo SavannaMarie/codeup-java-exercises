b0f234995a55b5e951de38437732d79f87eebdd7
Savanna Marie <savannamarie@gmail.com>
2021-05-25 09:03:38 -0500

    second exercise work

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -7,21 +7,21 @@ public class ConsoleExercises {
         System.out.printf("The value of pi is approximately: %.2f.%n", pi);
 
         Scanner newScanner = new Scanner(System.in);
-//        System.out.println("Please enter a number.");
-//        int userInput = newScanner.nextInt();
-//        System.out.printf("Your number was %d.%n", userInput);
-//
-//        System.out.println("Please enter 3 words.");
-//        String first = newScanner.next();
-//        String second = newScanner.next();
-//        String third = newScanner.next();
-//        System.out.printf("Your 3 words were: %s, %s, and %s%n", first, second, third);
-//
-//        newScanner.nextLine();
-//        System.out.println("Please enter a sentence:");
-//        String sentence = newScanner.nextLine();
-//        System.out.printf("your sentence was: %s", sentence);
-//        newScanner.nextLine();
+        System.out.println("Please enter a number.");
+        int userInput = newScanner.nextInt();
+        System.out.printf("Your number was %d.%n", userInput);
+
+        System.out.println("Please enter 3 words.");
+        String first = newScanner.next();
+        String second = newScanner.next();
+        String third = newScanner.next();
+        System.out.printf("Your 3 words were: %s, %s, and %s%n", first, second, third);
+
+        newScanner.nextLine();
+        System.out.println("Please enter a sentence:");
+        String sentence = newScanner.nextLine();
+        System.out.printf("your sentence was: %s", sentence);
+        newScanner.nextLine();
 
 
         System.out.println("Please enter the length of the classroom: ");

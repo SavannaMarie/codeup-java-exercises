09af20e09a861f30af4c9bef5cfdc72d05063af9
Savanna Marie <savannamarie@gmail.com>
2021-05-26 10:26:49 -0500

    control flow walkthrough

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -12,7 +12,7 @@ public class ControlFlowExercise {
 
 //        int i = 5;
 //        while (i <= 15){
-//            System.out.println(i++);
+//            System.out.print(i++ + " ");
 //        }
 
         /*======Do While
@@ -31,7 +31,7 @@ public class ControlFlowExercise {
 //            i += 2;
 //        } while (i <= 100);
 
-//        int i = 5;
+//        int i = 100;
 //        do {
 //            System.out.println(i);
 //            i -= 5;
@@ -45,10 +45,8 @@ public class ControlFlowExercise {
 
 
         /*Refactor the previous two exercises to use a for loop instead.*/
-//        for (int i = 5; i <= 15; i++){
-//            System.out.println(i);
-//        }
-//        for (int i = 0; i <= 100; i+= 2){
+
+//        for (int i = 100; i >= -10; i-=5){
 //            System.out.println(i);
 //        }
 //        for (long x = 2; x <= 1000000; x *= x){
@@ -74,11 +72,12 @@ public class ControlFlowExercise {
 
         Scanner scanner = new Scanner(System.in);
 //        System.out.print("Please enter pin: ");
-//        final int pin = 1234;
+//        final int pin = 12345;
 //        for (int i = 1; i <= 3; i++) {
 //            int attempt = scanner.nextInt();
 //            if (attempt == pin) {
 //                System.out.println("Correct, welcome back!");
+//                break;
 //            } else {
 //                System.out.println("Incorrect, try again.");
 //            } if (i == 3) {
@@ -93,22 +92,26 @@ public class ControlFlowExercise {
         Assume that the user will enter valid data.
         Only continue if the user agrees to.*/
 
-//        boolean response = true;
-//        do{
-//            System.out.println("What number would you like to go up to?");
-//            int number = scanner.nextInt();
-//            System.out.println("number! | squared! | cubed!");
-//            System.out.println("------ | ------- | -----");
-//            for(int i = 1; i <= number; i++) {
-//                System.out.print(i + " | " + (i * i) + " | " + (i * i * i));
-//                System.out.println();
-//            }
-//            System.out.println("Would you like to enter a number?");
-//            String answer = scanner.next().toLowerCase();
-//            if(!answer.equals("yes")) {
-//                response = false;
-//            }
-//        } while (response);
+        boolean response = true;
+        do{
+            System.out.println("What number would you like to go up to?");
+            int number = scanner.nextInt();
+            System.out.println("number! | squared! | cubed!");
+            System.out.println("------ | ------- | -----");
+            for(int i = 1; i <= number; i++) {
+                System.out.printf("%-6d", i);
+                System.out.print(" | ");
+                System.out.printf("%-7d", i * i);
+                System.out.print(" | ");
+                System.out.print(i * i * i);
+                System.out.println();
+            }
+            System.out.println("Would you like to enter a number?");
+            String answer = scanner.next().toLowerCase();
+            if(!answer.equals("yes")) {
+                response = false;
+            }
+        } while (response);
 
         /*
         Convert given number grades into letter grades.
@@ -119,20 +122,20 @@ public class ControlFlowExercise {
         The application should only continue if the user agrees to.
         */
 
-            System.out.println("Would you like to find out your grade?");
-            String answer = scanner.next().toLowerCase();
-            if (answer.equals("yes")) {
-                System.out.println("Enter your grade 0-100");
-                int grade = scanner.nextInt();
-                if ((grade >= 88) && (grade <= 100)) {
-                    System.out.println("Your grade is an A!");
-                } else if ((grade >= 80) && (grade <= 87)) {
-                    System.out.println("Your grade is a B!");
-                } else if ((grade >= 67) && (grade <= 79)) {
-                    System.out.println("Your grade is a C!");
-                } else if (grade <= 59) {
-                    System.out.println("Your grade is a F!");
-                }
-            }
+//            System.out.println("Would you like to find out your grade?");
+//            String answer = scanner.next().toLowerCase();
+//            if (answer.equals("yes")) {
+//                System.out.println("Enter your grade 0-100");
+//                int grade = scanner.nextInt();
+//                if ((grade >= 88) && (grade <= 100)) {
+//                    System.out.println("Your grade is an A!");
+//                } else if ((grade >= 80) && (grade <= 87)) {
+//                    System.out.println("Your grade is a B!");
+//                } else if ((grade >= 67) && (grade <= 79)) {
+//                    System.out.println("Your grade is a C!");
+//                } else if (grade <= 59) {
+//                    System.out.println("Your grade is a F!");
+//                }
+//            }
     }
 }

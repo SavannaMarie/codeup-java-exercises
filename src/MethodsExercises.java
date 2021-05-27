abaf2b3e53cf733ccb6c9579f5ecc67bd92d1ea6
Savanna Marie <savannamarie@gmail.com>
2021-05-27 11:33:54 -0500

    second part of exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static int addition(int num1, int num2){
         return num1 + num2;
@@ -14,6 +16,20 @@ public class MethodsExercises {
     public static int modulus(int num1, int num2){
         return num1 % num2;
     }
+    public static int getInteger(int min, int max){
+        System.out.printf("Give me a number between %d and %d\n", min, max);
+        Scanner scanner = new Scanner(System.in);
+        int userNumber = scanner.nextInt();
+
+        if ((userNumber > max) || (userNumber < min)) {
+            System.out.println("Try again!");
+            return getInteger(min, max);
+        } else {
+            System.out.printf("Thank you for following directions! Your number was %d!%n", userNumber);
+            return userNumber;
+        }
+
+    }
 
 
 
@@ -24,6 +40,8 @@ public class MethodsExercises {
         System.out.println(multiply(5, 5));
         System.out.println(division(10, 2));
         System.out.println(modulus(20, 3));
+
+        System.out.println(getInteger(1, 20));
 
     }
 }

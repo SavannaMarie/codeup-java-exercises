9fe730c4067447360def6b93a2d220a48401e459
Savanna Marie <savannamarie@gmail.com>
2021-06-01 15:20:45 -0500

    Objects exercise

--- src/Person.java
+++ src/Person.java
@@ -27,9 +27,26 @@ public class Person {
 //    public void person(){};
 
     public static void main(String[] args) {
-        Person newPerson = new Person("Savanna");
-//      newPerson.setName("Savanna2");
-        newPerson.sayHello();
+//        Person newPerson = new Person("Savanna");
+////        newPerson.setName("Savanna2");
+//        newPerson.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
 
 }

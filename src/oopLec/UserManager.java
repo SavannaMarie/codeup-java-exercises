088176264fea41972a1a870d124c2dd78c53ae86
Savanna Marie <savannamarie@gmail.com>
2021-06-01 13:23:20 -0500

    OOP Lecture

--- src/oopLec/UserManager.java
+++ src/oopLec/UserManager.java
@@ -4,14 +4,42 @@ import java.util.Scanner;
 public class UserManager {
     public static Scanner scanner = new Scanner(System.in);
 
+    public static void startMenu(User u) {
+        System.out.println("Welcome, would you like to do?");
+        System.out.println("1. Log in.\n2.Exit Program.");
+
+        int userChoice = scanner.nextInt();
+
+        if (userChoice == 1) {
+            UserTools.logIn(u);
+            editPreferences(u);
+        } else {
+            System.out.println("Thank you for your time. Bye!");
+        }
+    }
+
+    public static void editPreferences(User u) {
+        if(!u.isLoggedIn()) {
+            System.out.println("Sorry, you are currently logged out, please log in.");
+            UserTools.logIn(u);
+        } else {
+            System.out.println("Welcome to your preferences. Would you like to:\n1.Change your password.\n2.Log out");
+            int userChoice = scanner.nextInt();
+            System.out.println("You chose to choice " + userChoice);
+            switch (userChoice) {
+                case 1:
+                    UserTools.changePassword(u);
+                    editPreferences(u);
+                    break;
+                case 2:
+                    UserTools.logOut(u);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        User testUser = new User();
-        testUser.username = "SavannaMarie";
-        testUser.setPassword("password2");
-        testUser.email = "savanna@gmail.com";
-        System.out.println(testUser.getPassword());
+        User user2 = new User("Savanna2", "password2", "Savanna2@email.com");
 
-        UserTools.login(testUser);
-
+        startMenu(user2);
     }
 }
--- src/oopLec/UserTools.java
+++ src/oopLec/UserTools.java
@@ -5,31 +5,71 @@ import java.util.Scanner;
 public class UserTools {
     public static Scanner scanner = new Scanner(System.in);
 
-    public static void login(User user) {
+    public static void logIn(User u){
         System.out.println("Please enter your password: ");
 
         String userPassword = scanner.nextLine();
-        if(userPassword.equals(user.getPassword())){
-            System.out.println("You have successfully loged in!");
-        } else {
-            System.out.println("That password is incorrect");
-        }
 
+        if(userPassword.equals(u.getPassword())) {
+            u.setLoggedIn(true);
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("That password is incorrect.");
+        }
+    }
+
+    public static void logOut(User u){
+        System.out.println("Are you sure you want to log out? (Y/N)");
+
+        String userChoice = scanner.nextLine();
+
+        if(userChoice.equalsIgnoreCase("Y")) {
+            System.out.println("You have successfully logged out!");
+        } else {
+            System.out.println("We will keep you logged in.");
+        }
+    }
+
+    public static void changePassword(User u) {
+        if(!u.isLoggedIn()) {
+            System.out.println("Sorry, you are currently logged out. Please log in.");
+            logIn(u);
+        } else {
+            System.out.println("Please enter your current password: ");
+            String currentPassword = scanner.nextLine();
+            if(u.getPassword().equals(currentPassword)) {
+                System.out.println("Please enter a new password: ");
+                String newPassword = scanner.nextLine();
+                u.setPassword(newPassword);
+                System.out.println("Congratulations, your password is now " + newPassword);
+            } else {
+                System.out.println("Sorry, that password is incorrect.");
+            }
+        }
     }
 
     public static void main(String[] args) {
         User testUser = new User();
-        testUser.username = "SavannaMarie";
+        testUser.id = 1;
+        testUser.username = "Savanna";
+        // if password was public
+//        testUser.password = "password";
+        // if password is private
         testUser.setPassword("password");
-        testUser.email = "savanna@gmail.com";
-        System.out.println(testUser.getPassword());
+        testUser.email = "savanna@email.com";
+        logIn(testUser);
 
-        UserTools.login(testUser);
+//        User user2 = new User("codeup-dude", "gocodeup", "codeup@email.com");
+//
+//        System.out.println("Username: " + user2.username);
+//        System.out.println("Password: " + user2.getPassword());
+//        System.out.println("Email: " + user2.email);
+//
+////        user2.setPassword("testtesttest");
+//
+//        System.out.println(user2.getPassword());
 
+        changePassword(testUser);
 
-        User user2 = new User("Savanna2", "password2", "savanna2@gmail.com");
-        System.out.println("UserName: " + user2.username);
-        System.out.println("Password: " + user2.getPassword());
-        System.out.println("Email: " + user2.email);
     }
 }

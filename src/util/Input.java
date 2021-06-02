cb7b20c6678c2d9a24f694cd41a4d1308c719e31
Savanna Marie <savannamarie@gmail.com>
2021-06-01 21:56:51 -0500

    objects exercise

---
+++ src/util/Input.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString(){
+        return getString("Please enter a string");
+    }
+    public String getString(String inputString){
+        System.out.println(inputString);
+        return this.scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        return yesNo("Enter 'yes' or 'no' to find the boolean");
+    }
+    public boolean yesNo(String userBoolean){
+        System.out.print(userBoolean);
+        String input = this.scanner.nextLine();
+        System.out.print("Your answer is boolean ");
+        return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
+    }
+
+
+
+
+
+}
---
+++ src/util/InputTest.java
@@ -0,0 +1,13 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input stringInput = new Input();
+        System.out.println(stringInput.getString());
+        System.out.print(stringInput.yesNo() + "\n");
+
+
+
+
+    }
+}

70be9f1dfa76b444dda63b47b4d8fff4401fe616
Savanna Marie <savannamarie@gmail.com>
2021-06-01 22:06:50 -0500

    minor edit

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,7 +4,7 @@ public class InputTest {
     public static void main(String[] args) {
         Input stringInput = new Input();
         System.out.println(stringInput.getString());
-        System.out.print(stringInput.yesNo() + "\n");
+        System.out.print(stringInput.yesNo());
 
 
 

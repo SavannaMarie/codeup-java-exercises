5c85b88955fb808e2e7f96fedaaf1f3764a79f9a
Savanna Marie <savannamarie@gmail.com>
2021-06-07 14:33:53 -0500

    collections exercise first part complete

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -21,19 +21,51 @@ public class Student {
         this.name = name;
         this.grades = new ArrayList<>();
     }
+    public String getName() {
+        return this.name;
+    }
     public void addGrade(int grade) {
         grades.add(grade);
     }
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
     public double getGradeAverage() {
-
+        double total = 0.0;
+        for (Integer grade : grades) {
+            total = total + grade;
+        }
+        double gradesAdded = total / grades.size();
+        return gradesAdded;
     }
 
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         /*
         Verify that you can add grades to each object, and that your getGradeAverage method correctly returns the average of the student's grades.
          */
+
+        Student savanna = new Student("Savanna");
+        System.out.println("Grades for " + savanna.getName() + ": ");
+        savanna.addGrade(100);
+        savanna.addGrade(90);
+        savanna.addGrade(88);
+        savanna.addGrade(40);
+        System.out.println(savanna.getGrades());
+        System.out.println("=================");
+
+        System.out.print("Grade average for " + savanna.getName() + ": ");
+        System.out.print(savanna.getGradeAverage());
+
+
+        if (savanna.getGradeAverage() > 50 && savanna.getGradeAverage() < 80){
+            System.out.println("\nYou're making a C! I believe in you!");
+        } if (savanna.getGradeAverage() >= 80 && savanna.getGradeAverage() < 90){
+            System.out.println("\nAmazing!! You're making a B!! Impressive!");
+        } if (savanna.getGradeAverage() >= 90 && savanna.getGradeAverage() < 99){
+            System.out.println("You're making an A!! Excellent!");
+        }
 
     }
 }

abb48e6ed31f82a68e46b5d7c9af1cd17905b73f
Savanna Marie <savannamarie@gmail.com>
2021-06-10 16:55:11 -0500

    writing a file

--- src/FileIOLecture.java
+++ src/FileIOLecture.java
@@ -1,9 +1,16 @@
 import java.io.IOException;
 import java.nio.file.*;
+import java.io.*;
 
 public class FileIOLecture {
     public static void main(String[] args) {
         Path path = Paths.get("src", "FileIOLecture.java");
         System.out.println(path.toAbsolutePath());
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("/Users/savanna/Desktop/test/test.txt"));
+        } catch(Exception ex){
+            return;
+        }
     }
 }

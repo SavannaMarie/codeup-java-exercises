ccdc528579e7f73406c13fbc9868a8aea81f079e
Savanna Marie <savannamarie@gmail.com>
2021-06-24 13:45:54 -0500

    HashMap study

--- src/extra/hashmaps/HashStudy.java
+++ src/extra/hashmaps/HashStudy.java
@@ -5,17 +5,32 @@ import java.util.HashMap;
 public class HashStudy {
 
     public static void main(String[] args) {
-        int a = 10;
-        int b = 3;
-        int c = 88;
+//        int a = 10;
+//        int b = 3;
+//        int c = 88;
+//
+//
+//        HashMap<String, Integer> newHash = new HashMap<String, Integer>();
+//        newHash.put("a", 10);
+//        newHash.put("b", 3);
+//        newHash.put("c", 88);
+//
+//        System.out.println(newHash);
+//        System.out.println(newHash.get("c"));
+
+        HashMap<String, String> hash2 = new HashMap<String, String>();
+       hash2.put("Savanna", "savannaPassword123");
+       hash2.put("Marky", "pawPatrolDude");
+       hash2.put("Mark", "bestDadEver");
+
+        System.out.println(hash2);
+        System.out.println(hash2.containsValue("bestDadEver"));
+        System.out.println(hash2.containsKey("Mark"));
+        System.out.println(hash2.size());
+        hash2.replace("Marky", "BabySharkDoDoDo");
+        System.out.println(hash2);
+        System.out.println(hash2.keySet());
 
 
-        HashMap<String, Integer> newHash = new HashMap<String, Integer>();
-        newHash.put("a", 10);
-        newHash.put("b", 3);
-        newHash.put("c", 88);
-
-        System.out.println(newHash);
-        System.out.println(newHash.get("c"));
     }
 }
